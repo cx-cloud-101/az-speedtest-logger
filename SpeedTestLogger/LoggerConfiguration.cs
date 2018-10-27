c71e854aada38ff1d15d7c42646cc67610e8d641
teodoran <teodor@andeelstad.com>
2018-10-27 17:08:50 +0200

    ServiceBus scheduling

--- SpeedTestLogger/LoggerConfiguration.cs
+++ SpeedTestLogger/LoggerConfiguration.cs
@@ -12,11 +12,14 @@ namespace SpeedTestLogger
         public readonly RegionInfo LoggerLocation;
         public readonly Uri ApiUrl;
 
+        public readonly ServiceBusConfiguration ServiceBus;
+
         public LoggerConfiguration()
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json");
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Development.json");
 
             var configuration = builder.Build();
 
@@ -24,6 +27,22 @@ namespace SpeedTestLogger
             LoggerId = Int32.Parse(configuration["loggerId"]);
             LoggerLocation = new RegionInfo(configuration["loggerLocationCountryCode"]);
             ApiUrl = new Uri(configuration["speedTestApiUrl"]);
+
+            ServiceBus = new ServiceBusConfiguration(configuration);
+        }
+    }
+
+    public class ServiceBusConfiguration
+    {
+        public readonly string ConnectionString;
+        public readonly string TopicName;
+        public readonly string SubscriptionName;
+
+        public ServiceBusConfiguration(IConfigurationRoot configuration)
+        {
+            ConnectionString = configuration["serviceBus:connectionString"];
+            TopicName = configuration["serviceBus:topicName"];
+            SubscriptionName = configuration["serviceBus:subscriptionName"];
         }
     }
 }
--- SpeedTestLogger/Program.cs
+++ SpeedTestLogger/Program.cs
@@ -1,36 +1,59 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
 using SpeedTestLogger.Models;
 
 namespace SpeedTestLogger
 {
     class Program
-    {   
+    {
+        private static LoggerConfiguration _config;
+        private static SubscriptionClient _subscriptionClient;
+
         static async Task Main()
         {
             Console.WriteLine("Starting SpeedTestLogger");
-            var config = new LoggerConfiguration();
+            Console.WriteLine("Press any key to exit");
 
-            Console.WriteLine("Running new speedtest");
-            var runner = new SpeedTestRunner(config.LoggerLocation);
+            _config = new LoggerConfiguration();
+            var options = new MessageHandlerOptions(HandleException)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = true
+            };
+            _subscriptionClient = new SubscriptionClient(_config.ServiceBus.ConnectionString, _config.ServiceBus.TopicName, _config.ServiceBus.SubscriptionName);
+            _subscriptionClient.RegisterMessageHandler(HandleSpeedTestMessage, options);
+
+            Console.ReadKey();
+
+            await _subscriptionClient.CloseAsync();
+        }
+
+        static async Task HandleSpeedTestMessage(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Received message: {Encoding.UTF8.GetString(message.Body)}");
+
+            var runner = new SpeedTestRunner(_config.LoggerLocation);
             var testData = runner.RunSpeedTest();
 
             Console.WriteLine("Got download: {0} Mbps and upload: {1} Mbps", testData.Speeds.Download, testData.Speeds.Upload);
             var results = new TestResult
             {
-                User = config.UserId,
-                Device = config.LoggerId,
+                User = _config.UserId,
+                Device = _config.LoggerId,
                 Timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
                 Data = testData
             };
 
             Console.WriteLine("Uploading data to speedtest API");
             var success = false;
-            using (var client = new SpeedTestApiClient(config.ApiUrl))
+            using (var client = new SpeedTestApiClient(_config.ApiUrl))
             {
                 success = await client.PublishTestResult(results);    
             }
-            
+
             if (success)
             {
                 Console.WriteLine("Speedtest complete!");
@@ -38,6 +61,13 @@ namespace SpeedTestLogger
             else {
                 Console.WriteLine("Speedtest failed!");
             }
+        }
+
+        static Task HandleException(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+
+            return Task.CompletedTask;
         }
     }
 }
